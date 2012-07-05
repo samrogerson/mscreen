b9c514c80d3109fb98178db9cfaa478a33926146
Sam Rogerson <samr@cern.ch>
2012-07-05 01:54:36 +0100

    basic dropdowns per screen

--- modules/ui.py
+++ modules/ui.py
@@ -1,21 +1,30 @@
 import tkinter as tk
 
-def spawn_screen():
+#def spawn_screen():
+    #def reply():
+        #from tkinter.messagebox import showinfo
+        #showinfo(title='popup', message='button pressed')
 
-    def reply():
-        from tkinter.messagebox import showinfo
-        showinfo(title='popup', message='button pressed')
+def print_it(event):
+    print(event)
+    #print(var.get())
 
-    def print_it(event):
-        print(var.get())
-
+def make_option_menus(screens):
     window = tk.Tk()
-    button = tk.Button(window, text='press', command=reply)
-    button.pack()
+    #button = tk.Button(window, text='press', command=reply)
+    #button.pack()
 
-    var = tk.StringVar()
-    var.set('a')
-    omenu = tk.OptionMenu(window, var, 'a','b','c', command=print_it)
-    omenu.pack()
+    vars = []
+    omenus = []
+    print(dir(screens))
+    for screen_name, modes  in screens.items():
+        vars.append(tk.StringVar())
+        vars[-1].set(screen_name+"...")
+        options = [ "{res} ({ref})".format(res=res,ref=ref) for res,ref in
+                    zip(modes['resolutions'], modes['refresh_rates']) ]
+        omenus.append(tk.OptionMenu(window, vars[-1],
+                                    *options,
+                                    command=print_it))
+        omenus[-1].pack()
 
     window.mainloop()
--- modules/xrandr.py
+++ modules/xrandr.py
@@ -1,5 +1,7 @@
+from subprocess import check_output
+from collections import defaultdict
 
-def get_screens(screen_types = ["LVDS", "VGA"]):
+def get_screens(preferred_only = False):
     xrandr_out = check_output('xrandr')
     xrandr = str(xrandr_out, encoding='utf8').split('\n')
 
@@ -8,17 +10,25 @@ def get_screens(screen_types = ["LVDS", "VGA"]):
 
     for line_num, line in enumerate(xrandr):
         fields = line.split()
-        is_screen_type = [ pos for pos, scr in enumerate(screen_types)
-                           if line.startswith(scr)]
-        if len(is_screen_type) > 0 and 'connected' in fields:
+        is_screen_type = not line.startswith(' ') and len(line) > 0
+        if is_screen_type and 'connected' in fields:
             screen_type = fields[0]
             current_screen_type = screen_type
-        elif current_screen_type is not None:
+            screens[current_screen_type] = defaultdict(list)
+        elif current_screen_type is not None and 'disconnected' not in fields:
             # we've actually found a screen already, so this is a res line
-            if len(fields) > 0 and "+" in fields[-1]:
-                # this is the preferred mode
+            if len(fields) > 0:
                 res = fields[0]
                 ref = fields[1].rstrip('+*')
-                screens[current_screen_type]['resolution'] = res
-                screens[current_screen_type]['refresh_rate'] = ref
+                if "+" in fields[-1]:
+                    # this is the preferred mode so we want it at the start of
+                    # the list
+                    c_res = screens[current_screen_type]['resolutions']
+                    c_ref = screens[current_screen_type]['refresh_rates']
+                    screens[current_screen_type]['resolutions'] = c_res + [res]
+                    screens[current_screen_type]['refresh_rates'] = (c_ref +
+                                                                     [ref])
+                elif not preferred_only:
+                    screens[current_screen_type]['resolutions'].append(res)
+                    screens[current_screen_type]['refresh_rates'].append(ref)
     return screens
