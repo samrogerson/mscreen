32d1ca065e7e84dd493075962143390da825d6dd
Sam Rogerson <samr@cern.ch>
2012-07-06 09:58:14 +0100

    example geom tk

--- modules/ui.py
+++ modules/ui.py
@@ -1,18 +1,38 @@
-import tkinter as tk
+#!/usr/bin/python
 
-#def spawn_screen():
-    #def reply():
-        #from tkinter.messagebox import showinfo
-        #showinfo(title='popup', message='button pressed')
+from tkinter import Tk, RIGHT, BOTH, RAISED, Frame, Button, ttk
+from tkinter.ttk import Style
 
-def print_it(event):
-    print(event)
-    #print(var.get())
+class xrUI(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.parent.title("Buttons")
+        self.style = Style()
+        self.style.theme_use("default")
+
+        frame = Frame(self, relief=RAISED, borderwidth=1)
+        frame.pack(fill=BOTH, expand=1)
+
+        self.pack(fill=BOTH, expand=1)
+
+        closeButton = Button(self, text="Close")
+        closeButton.pack(side=RIGHT, padx=5, pady=5)
+        okButton = Button(self, text="OK")
+        okButton.pack(side=RIGHT)
+
 
 def make_option_menus(screens):
     window = tk.Tk()
-    #button = tk.Button(window, text='press', command=reply)
-    #button.pack()
+    window.geometry("300x200+300+300")
+    app = xrUI(window)
 
     vars = []
     omenus = []
@@ -22,8 +42,8 @@ def make_option_menus(screens):
         options = [ "{res} ({ref})".format(res=res,ref=ref) for res,ref in
                     zip(modes['resolutions'], modes['refresh_rates']) ]
         omenus.append(tk.OptionMenu(window, vars[-1],
-                                    *options,
-                                    command=print_it))
+                                    *options))
+                                    #command=print_it))
         omenus[-1].pack()
 
     window.mainloop()
