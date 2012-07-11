62901e747bb7d05efb8c89adc8fe07cc8b899105
Sam Rogerson <samr@cern.ch>
2012-07-11 10:47:37 +0100

    go to script style

--- modules/ui.py
+++ modules/ui.py
@@ -4,32 +4,11 @@ import tkinter as tk
 from tkinter import Tk, RIGHT, BOTH, RAISED, Frame, Button, ttk
 from tkinter.ttk import Style
 
-class xrUI(Frame):
+print(dir(tk))
 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
 
-        self.parent = parent
 
-        self.initUI()
-
-    def initUI(self):
-
-        self.parent.title("Buttons")
-        self.style = Style()
-        self.style.theme_use("default")
-
-        frame = Frame(self, relief=RAISED, borderwidth=1)
-        frame.pack(fill=BOTH, expand=1)
-
-        self.pack(fill=BOTH, expand=1)
-
-        closeButton = Button(self, text="Close")
-        closeButton.pack(side=RIGHT, padx=5, pady=5)
-        okButton = Button(self, text="OK")
-        okButton.pack(side=RIGHT)
-
-def add_option_menus(screens, window):
+def add_option_menus(screens, frame):
     vars = []
     omenus = []
     for screen_name, modes  in screens.items():
@@ -37,14 +16,27 @@ def add_option_menus(screens, window):
         vars[-1].set(screen_name+"...")
         options = [ "{res} ({ref})".format(res=res,ref=ref) for res,ref in
                     zip(modes['resolutions'], modes['refresh_rates']) ]
-        omenus.append(tk.OptionMenu(window, vars[-1],
-                                    *options))
+        omenus.append(tk.OptionMenu(frame, vars[-1], *options))
         omenus[-1].pack()
 
 def gen_ui(screens):
     window = Tk()
+# size + offset
     window.geometry("300x200+300+300")
-    add_option_menus(screens, window)
+    window.title("mscreen")
 
-    app = xrUI(window)
+
+    xr_ui = Frame(window)
+    xr_ui.pack(fill=BOTH,expand=1)
+    add_option_menus(screens, xr_ui)
+
+    sel_buttons = Frame(window)
+    frame = Frame(sel_buttons, relief=RAISED, borderwidth=1)
+    frame.pack(fill=BOTH, expand=1)
+    sel_buttons.pack(fill=BOTH, expand=1)
+    closeButton = Button(sel_buttons, text="Close")
+    closeButton.pack(side=RIGHT, padx=5, pady=5)
+    okButton = Button(sel_buttons, text="OK")
+    okButton.pack(side=RIGHT)
+
     window.mainloop()
